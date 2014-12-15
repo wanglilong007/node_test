499d9a36c397db39d38a7d3bc131a1627c65d3d6
wanglilong <wanglilong@huawei.com>
2014-12-15 11:11:33 +0800

    add a route

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res){
-	res.send('hello, it is a node test');
+app.get('/:filename', function(req, res, next){
+	var option = {
+		root: __dirname+'/public',
+	};
+	var filename = req.params.filename;
+	if (filename){
+			res.sendFile(filename, option, function(err){
+			if (err){
+				console.log(err);
+				res.status(err.status).end();
+			}
+		});
+	} else {
+		res.sendFile('index.html', option);
+	}
+	
 	console.log(req.ip);
 	console.log(req.protocol);
 });
